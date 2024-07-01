9392fc7052bae212ac6b25788059a86446fe85b3
ViktoriaFedyunina <vika-0607@yandex.ru>
2024-07-01 15:20:06 +0300

    2.9. Вкатываем кнопки слайдера

--- source/js/modules/footer.js
+++ source/js/modules/footer.js
@@ -6,7 +6,11 @@ export default () => {
       footerTogglers[i].addEventListener(`click`, function () {
         let footer = footerTogglers[i].parentNode;
         if (footer.classList.contains(`screen__footer--full`)) {
-          footer.classList.remove(`screen__footer--full`);
+          footer.classList.add(`screen__footer--before-hide`);
+          setTimeout(() => {
+            footer.classList.remove(`screen__footer--full`);
+            footer.classList.remove(`screen__footer--before-hide`);
+          }, 900);
         } else {
           footer.classList.add(`screen__footer--full`);
         }
